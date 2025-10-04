0f3494c77ae6d21a76c44f6d98d497b11827785f
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2025-10-04 14:18:41 +0200

    Process video file in chunks.

--- src/rendererWorker.ts
+++ src/rendererWorker.ts
@@ -2,19 +2,21 @@ import type { WorkerMsg } from "./workerMsg";
 import * as MP4Box from "mp4box";
 
 interface RenderState {
-  lastTime: number;
   videoDecoder: VideoDecoder;
+  framesInFlight: Array<VideoFrame>;
+  lastTime?: number;
   canvas?: OffscreenCanvas;
   canvasCtx?: OffscreenCanvasRenderingContext2D;
 }
-
+const FRAME_BUDGET = 1000 / 30;
+const MAX_FRAMES_IN_FLIGHT = 2;
+const chunkSize = 16 << 10; // 16KB chunk sizes
 const videoDecoderInit: VideoDecoderInit = {
-  output: renderFrame,
+  output: handleVideoFrame,
   error: (e) => console.log(e.message)
 }
-
 const renderState: RenderState = {
-  lastTime: 0,
+  framesInFlight: new Array(MAX_FRAMES_IN_FLIGHT),
   videoDecoder: new VideoDecoder(videoDecoderInit),
 }
 
@@ -31,8 +33,11 @@ onmessage = (ev) => {
       break;
     }
     case "Video": {
-      const file = data
-      readFile(file);
+      const file = data;
+      // Call readFile asynchronously so it doesn't block the event loop
+      (async () => {
+        await readFile(file);
+      })();
       break;
     }
     default:
@@ -57,7 +62,25 @@ function onVideoSample(id: number, user: unknown, samples: MP4Box.Sample[]) {
     duration: sample.duration,
     timestamp: sample.dts
   })
+
   renderState.videoDecoder.decode(chunk);
+
+  // const waitForDecoder = new Promise<void>((resolve) => {
+  //   if (renderState.videoDecoder.decodeQueueSize < MAX_FRAMES_IN_FLIGHT) {
+  //     resolve();
+  //   }
+  //   else {
+  //     renderState.videoDecoder.ondequeue = (_e) => {
+  //       if (renderState.videoDecoder.decodeQueueSize < MAX_FRAMES_IN_FLIGHT) {
+  //         resolve();
+  //       }
+  //     }
+  //   }
+  // });
+
+  // waitForDecoder.then(() =>
+  //   renderState.videoDecoder.decode(chunk)
+  // );
 }
 
 function onMoovParsed(info: MP4Box.Movie) {
@@ -68,30 +91,84 @@ function onMoovParsed(info: MP4Box.Movie) {
     codedWidth: videoTrack.track_width,
     codec: videoTrack.codec
   }
-  // TODO(K): Haven't figured out how to check if the config is supported.
-  // For now, we just assume it works
-  // VideoDecoder.isConfigSupported(config).then((supported) => {
-  //   console.assert(supported.supported);
-  // });
+
   renderState.videoDecoder.configure(config);
 
   mp4box.setExtractionOptions(videoTrack.id, videoTrack, { nbSamples: 1 })
   mp4box.start();
 }
 
-function readFile(file: File) {
-  const fileReader = new FileReader();
-  fileReader.onload = (ev) => {
-    const buf = ev.target?.result as ArrayBuffer;
-    const buffer = MP4Box.MP4BoxBuffer.fromArrayBuffer(buf, 0);
-    mp4box.appendBuffer(buffer);
-  }
-  fileReader.readAsArrayBuffer(file)
+function readChunk(file: File, chunkSize: number, offset: number) {
+  const chunkBlob = file.slice(offset, offset + chunkSize);
+  const chunk = chunkBlob.arrayBuffer();
+
+  return chunk
 }
 
+function getNextChunk(file: File, chunkSize: number, offset: number) {
+  return new Promise<ArrayBuffer>((resolve) => {
+    if (renderState.videoDecoder.decodeQueueSize < MAX_FRAMES_IN_FLIGHT) {
+      resolve(readChunk(file, chunkSize, offset));
+    }
+    else {
+      renderState.videoDecoder.ondequeue = (_e) => {
+        if (renderState.videoDecoder.decodeQueueSize < MAX_FRAMES_IN_FLIGHT) {
+          resolve(readChunk(file, chunkSize, offset))
+        }
+      };
+    }
+  });
+}
 
-function renderFrame(frame: VideoFrame) {
+async function readFile(file: File) {
+  const chunks = ((file.size + chunkSize - 1) / chunkSize) >> 0
+  let offset = 0;
+  for (let i = 0; i < chunks; i++) {
+
+    // First wait for there to be resources to process more frames
+    // const chunk = await getNextChunk(file, chunkSize, offset);
+    const chunk = await readChunk(file, chunkSize, offset);
+    const buffer = MP4Box.MP4BoxBuffer.fromArrayBuffer(chunk, offset);
+    offset = mp4box.appendBuffer(buffer);
+  }
+  mp4box.flush();
+}
+
+function handleVideoFrame(frame: VideoFrame) {
+  renderState.framesInFlight.push(frame)
+  setTimeout(renderLoop, 0);
+}
+
+async function renderLoop() {
+  if (renderState.framesInFlight.length < 1) {
+    return;
+  }
+  const frame = renderState.framesInFlight.shift()!;
   const { width, height } = renderState.canvas!;
   renderState.canvasCtx?.drawImage(frame, 0, 0, width, height);
   frame.close();
+
+  setTimeout(renderLoop, 0);
 }
+
+
+
+
+// function renderFrame(frame: VideoFrame) {
+//   const { width, height } = renderState.canvas!;
+//   const now = performance.now();
+//   if (renderState.lastTime === undefined) {
+//     renderState.lastTime = now;
+//     renderState.canvasCtx?.drawImage(frame, 0, 0, width, height);
+//     frame.close();
+//   }
+//   else {
+//     const diff = now - renderState.lastTime;
+//     renderState.lastTime = now;
+//     const sleep = FRAME_BUDGET - diff;
+//     setTimeout(() => {
+//       renderState.canvasCtx?.drawImage(frame, 0, 0, width, height);
+//       frame.close();
+//     }, 33);
+//   }
+// }
