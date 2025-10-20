a6653107dee81326594fd39b43f820820939bece
Kristoffer Kortbaek <kristoffer@kortbaek.dk>
2025-10-20 22:24:10 +0200

    Understand my own styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import styles from './controls.module.css';
 import { PlayPause, Replay, SelectVideo } from './VideoControls';
 import { DrawingSurface } from './DrawingSurface';
- 
+
 
 function App() {
   return (
-    <div id="container" >
-      <DrawingSurface />      
+    <div id="app">    
+      <div id="container" >
+        <DrawingSurface />
+      </div>
       <div className={styles.controlsContainer}>
         <SelectVideo />
         <PlayPause />
